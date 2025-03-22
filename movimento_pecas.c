35f22de3998ef83fa0a0e432311f23662353d460
alfredo601 <61746409+alfredo601@users.noreply.github.com>
2025-03-22 11:22:01 +0000

    Inicio do desafio

---
+++ movimento_pecas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // movimento da torre
+    // Torre: 5 casas para a direita
+
+    int casas = 5;
+    
+    for (int i = 1; i <= casas; i++) {
+        printf("%d Direita \n", i);
+    }
+
+}
